e73fd3137876e6cb7b0a5dffbb42261a07e7ea0b
Dustin Farist <dustinfarist@users.noreply.github.com>
2018-08-01 11:48:29 -0400

    Remove Effect[F] requirement in ObjectOps and BucketOps impls

--- s3/src/main/scala/com/github/rstradling/awsio/s3/BucketOpsAwsImpl.scala
+++ s3/src/main/scala/com/github/rstradling/awsio/s3/BucketOpsAwsImpl.scala
@@ -1,8 +1,9 @@
 package com.github.rstradling.awsio.s3
 
-import cats.~>
-import cats.effect.Effect
-import cats.implicits._
+import cats.{~>, ApplicativeError}
+import cats.syntax.applicativeError._
+import cats.syntax.functor._
+import cats.syntax.option._
 import java.util.concurrent.CompletableFuture
 import software.amazon.awssdk.services.s3.S3AsyncClient
 import software.amazon.awssdk.services.s3.model._
@@ -11,11 +12,11 @@ import software.amazon.awssdk.services.s3.model._
   * Implementation of the aws bucket operations using an S3 async client.
   * This client uses a Java 8 CompleteableFuture
   * @param client - The s3 Async client
-  * @param f - Effect must support cats.effect.Effect operations
+  * @param F - F[_] must support cats.ApplicativeError[F, Throwable] for .recover
   * @param transform - Natural transformation from a CompletableFuture to an F
   * @tparam F - The effect type to use like Monix Task or IO
   */
-class BucketOpsAwsImpl[F[_]](client: S3AsyncClient)(implicit f: Effect[F], transform: CompletableFuture ~> F) extends BucketOps [F] {
+class BucketOpsAwsImpl[F[_]](client: S3AsyncClient)(implicit F: ApplicativeError[F, Throwable], transform: CompletableFuture ~> F) extends BucketOps[F] {
   def create(createBucketRequest: CreateBucketRequest): F[CreateBucketResponse] = {
     transform(client.createBucket(createBucketRequest))
   }
@@ -28,12 +29,9 @@ class BucketOpsAwsImpl[F[_]](client: S3AsyncClient)(implicit f: Effect[F], trans
   def exists(headBucketRequest: HeadBucketRequest): F[Option[HeadBucketResponse]] = {
     transform(client.headBucket(headBucketRequest)).map(x => x.some).recover {
       // Docs say a NoSuchBucketException should occur for a bucket that does
-      // not exist.  That does not seem to be true and instead a 404 S3Exception
-      // is thrown
-      case _: NoSuchBucketException =>
-        None: Option[HeadBucketResponse]
-      case s: S3Exception if s.statusCode == 404 =>
-        None: Option[HeadBucketResponse]
+      // not exist. That does not seem to be true and instead a 404 S3Exception is thrown
+      case _: NoSuchBucketException => none[HeadBucketResponse]
+      case s: S3Exception if s.statusCode == 404 => none[HeadBucketResponse]
     }
   }
 }
--- s3/src/main/scala/com/github/rstradling/awsio/s3/ObjectOpsAwsImpl.scala
+++ s3/src/main/scala/com/github/rstradling/awsio/s3/ObjectOpsAwsImpl.scala
@@ -1,14 +1,12 @@
 package com.github.rstradling.awsio.s3
 
 import cats.~>
-import cats.effect.Effect
 import java.util.concurrent.CompletableFuture
-import software.amazon.awssdk.core.async.AsyncRequestBody
-import software.amazon.awssdk.core.async.AsyncResponseTransformer
+import software.amazon.awssdk.core.async.{AsyncRequestBody, AsyncResponseTransformer}
 import software.amazon.awssdk.services.s3.S3AsyncClient
 import software.amazon.awssdk.services.s3.model._
 
-class ObjectOpsAwsImpl[F[_]](client: S3AsyncClient)(implicit f: Effect[F], transform: CompletableFuture ~> F) extends ObjectOps [F] {
+class ObjectOpsAwsImpl[F[_]](client: S3AsyncClient)(implicit transform: CompletableFuture ~> F) extends ObjectOps[F] {
   def put(putObjectRequest: PutObjectRequest, body: AsyncRequestBody): F[PutObjectResponse] = {
     transform(client.putObject(putObjectRequest, body))
   }
